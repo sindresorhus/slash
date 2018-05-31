e839a34d3c897902368d43ce83c0fde477f4b6de
小虎Oni <xiaohuoni@users.noreply.github.com>
2018-06-01 00:33:38 +0800

    has Chinese characters
    fix Naming with Chinese

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 module.exports = input => {
 	const isExtendedLengthPath = /^\\\\\?\\/.test(input);
 	const hasNonAscii = /[^\u0000-\u0080]+/.test(input); // eslint-disable-line no-control-regex
-
-	if (isExtendedLengthPath || hasNonAscii) {
+        const hasChinese = /[^\u4e00-\u9fa5]+/.test(input);  // has Chinese characters
+	
+	if (isExtendedLengthPath || (hasNonAscii&&!hasChinese)) {
 		return input;
 	}
 
