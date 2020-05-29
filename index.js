def09fb1ece2c4ec76ca1bf0f5764f909b026288
Mohamed Nainar <mdnainar9615@gmail.com>
2020-05-29 23:42:20 +0530

    onwe

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ module.exports = path => {
 	const isExtendedLengthPath = /^\\\\\?\\/.test(path);
 	const hasNonAscii = /[^\u0000-\u0080]+/.test(path); // eslint-disable-line no-control-regex
 
-	if (isExtendedLengthPath || hasNonAscii) {
-		return path;
-	}
+	if (isExtendedLengthPath || hasNonAscii) return path;
 
 	return path.replace(/\\/g, '/');
 };
