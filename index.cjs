8cb80d03f21d8b2e92ff8d9fdab0e1aaaa3ff384
markthree <1801982702@qq.com>
2022-01-21 20:05:59 +0800

    feat: commonjs support

---
+++ index.cjs
@@ -0,0 +1,12 @@
+function slash(path) {
+	const isExtendedLengthPath = /^\\\\\?\\/.test(path);
+	const hasNonAscii = /[^\u0000-\u0080]+/.test(path); // eslint-disable-line no-control-regex
+
+	if (isExtendedLengthPath || hasNonAscii) {
+		return path;
+	}
+
+	return path.replace(/\\/g, "/");
+}
+
+module.exports = slash
