a64c2947e50c9566b00719317846c425073eb732
Johan Fagerberg <johanringmann@gmail.com>
2022-09-26 10:10:23 +0200

    tests: update tests to reflect support of ASCII paths

--- test.js
+++ test.js
@@ -4,14 +4,10 @@ import slash from './index.js';
 test('convert backwards-slash paths to forward slash paths', t => {
 	t.is(slash('c:/aaaa\\bbbb'), 'c:/aaaa/bbbb');
 	t.is(slash('c:\\aaaa\\bbbb'), 'c:/aaaa/bbbb');
+	t.is(slash('c:\\aaaa\\bbbb\\★'), 'c:/aaaa/bbbb/★');
 });
 
 test('not convert extended-length paths', t => {
 	const path = '\\\\?\\c:\\aaaa\\bbbb';
-	t.is(slash(path), path);
-});
-
-test('not convert paths with Unicode', t => {
-	const path = 'c:\\aaaa\\bbbb\\★';
 	t.is(slash(path), path);
 });
