9532dfb121c3d6e27679e68968d4beee341fdb0b
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2023-04-25 19:26:50 +0100

    Fix TypeScript doc comment (#23)

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /**
 Convert Windows backslash paths to slash paths: `foo\\bar` ➔ `foo/bar`.
 
-[Forward-slash paths can be used in Windows](http://superuser.com/a/176395/6877) as long as they're not extended-length paths and don't contain any non-ascii characters.
+[Forward-slash paths can be used in Windows](http://superuser.com/a/176395/6877) as long as they're not extended-length paths.
 
 @param path - A Windows backslash path.
 @returns A path with forward slashes.
